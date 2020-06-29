9bc33b3b7fe28b7c8df11b7b2c6871be5cdafda6
Jr Bytes <itamazor@gmail.com>
2020-06-29 15:29:41 -0300

    Added loading file md.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Markdown from 'markdown-to-jsx'
+import React, { useEffect, useState } from 'react'
+import md from './posts/arquivo.md'
 
 function App() {
+  const [textMD, setTextMD] = useState('')
+
+  useEffect(() => {
+    fetch(md)
+      .then(res => res.text())
+      .then(res => setTextMD(res))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Markdown
+        children={textMD}
+      />
     </div>
   );
 }
---
+++ src/posts/arquivo.md
@@ -0,0 +1,8 @@
+# Título do arquivo
+
+```js
+  const name = 'JrBytes'
+  console.log(name)
+```
+
+Uma frase para testar **negrito** e *itálico*.
